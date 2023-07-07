1acf8f317f1b58918f546238c5c4b8674b0829b1
roiavivi <67497305+roiavivi@users.noreply.github.com>
2023-07-07 10:47:30 +0300

    Update s3-state-lock.tf

--- Section 5 - Remote State Management/s3-state-lock.tf
+++ Section 5 - Remote State Management/s3-state-lock.tf
@@ -13,3 +13,8 @@ resource "time_sleep" "wait_150_seconds" {
 
   create_duration = "150s"
 }
+
+###
+# AWS
+open DynamoDB > create new table > name the table "terraform-state-locking" > the table must hav a partition key named "LockID" with the type of String
+###
